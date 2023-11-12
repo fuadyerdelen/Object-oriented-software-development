74abe8400fb5c0a9ebca461b1192f3b1591e835d
m.fuad <m.fuadyerdelen@gmail.com>
2023-11-12 15:54:36 +0100

    Die Erklärungen von Aufgabe3 mit Kommenter

--- Übung3/Aufgabe3.java
+++ Übung3/Aufgabe3.java
@@ -1,15 +1,16 @@
 package Übung3;
+
 public class Aufgabe3 {
 
     // Aufgabe3.a
     static void sumEvenNums(int z) {
-        int ergebnis = 0;	
+        int ergebnis = 0;
         for (int i = 1; i <= z; i++) {
 
             ergebnis += i; // ergebnis = ergebnis + i;
         }
-       System.out.println(ergebnis);
-        
+        System.out.println(ergebnis);
+
     }
 
     // Aufgabe3.b
@@ -17,23 +18,28 @@ public class Aufgabe3 {
         int erg = 0;
 
         for (int i = 1; i <= z; i++) {
+            // Hier defienire ich if bedingung für die blnSumOdds, ob es true oder false ist
             if (blnSumOdds == true) {
+                // Wenn es false ist addiere ich den alle gerade Zahlen bis Varieable z
                 if (i % 2 == 0) {
-                    erg += i; 
+                    erg += i; // erg = erg + i;
                 }
-				
+                // Hier defienire ich if bedingung für die blnSumOdds, ob es true oder false ist
+
             } else if (blnSumOdds == false) {
+                // Wenn es false ist addiere ich den alle ungerade Zahlen bis Varieable z
                 if (i % 2 != 0) {
-                    erg += i;
+                    erg += i; // erg = erg + i;
                 }
-				
+
             }
         }
 
-        System.err.println(erg);
+        System.out.println(erg);
 
     }
 
+    // In main Method rufe ich den Methoden mit Paremeters auf
     public static void main(String[] args) {
         sumEvenNums(6);
         sumEvenOrOddNums(9, true);
