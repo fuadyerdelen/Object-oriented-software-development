f6c53fc783b7a4f2c57acffd9546e9afa7518d92
m.fuad <m.fuadyerdelen@gmail.com>
2023-11-19 21:52:53 +0100

    Probelösung von Aufgabe2.g. Etwas fehlt noch

--- Übung4/Aufgabe2.java
+++ Übung4/Aufgabe2.java
@@ -1,15 +1,28 @@
 
 package Übung4;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Aufgabe2 {
 
-    // Aufgbabe 2.d
     public static void main(String[] args) {
-        //add(num());
+        // Aufgbabe 2.d
+        // add(num());
 
-        System.out.println(find(3));
+        // Aufgabe 2.f
+        // System.out.println(find(3));
+
+        remove(3);
+        System.out.println(remove(3));
+        System.out.println("Erhaltene Feldwerte: ");
+
+        for (int i = 0; i < feld.length; i++) {// ------> für Ausgabe von Array
+            System.out.print(feld[i]);
+            if (i < feld.length - 1) {
+                System.out.print(", ");
+            }
+        }
+
     }
 
     // Aufgabe 2.c
@@ -23,7 +36,7 @@ public class Aufgabe2 {
     }
 
     // Aufgabe 2.a
-    public static int[] feld = new int[0]; //{ 1, 2, 3, 4, 5 }; ---> zum testen
+    public static int[] feld = { 1, 2, 3, 4, 5 }; // new int[0]; ---> zum testen
 
     // Aufgabe 2.b
     public static void add(int z) {
@@ -38,19 +51,39 @@ public class Aufgabe2 {
         // System.out.println(f1[f1.length - 1]); //-----> zum testen
     }
 
-    //Aufgabe 2.e
+    // Aufgabe 2.e
     public static int find(int z) {
 
         for (int i = 0; i < feld.length; i++) {
 
             if (z == feld[i]) { // ----> Wenn z gleich Element von feld
-                return i + 1; // -----> Index von z in feld "i+1", weil Arrays immer mit 0 beginnen
+                return i; // -----> Index von z in feld
             }
         }
 
         return -1;
     }
 
+    // Aufgabe 2.g
+    public static boolean remove(int z) {
 
+        int findIndex = find(z);
+
+        int[] f1 = new int[feld.length - 1];
+
+        // Koppie-schleife
+        for (int i = 0; i < feld.length; i++) {
+
+            if (i == findIndex) {
+                f1[i+1] = feld[i];
+
+                return true;
+            }
+            f1[i] = feld[i];
+        }
+
+        feld = f1;
+        return false;
+    }
 
 }
