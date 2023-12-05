27c73ce9a6bebc0231c423e08f324ee84d44b890
m.fuad <m.fuadyerdelen@gmail.com>
2023-12-05 16:59:22 +0100

    Die komplette Lösung von Aufgabe2

--- Übung6/Gladiator.java
+++ Übung6/Gladiator.java
@@ -12,7 +12,8 @@ public class Gladiator {
 
     public Gladiator(String name, int at, int pa, int rs, int le) {
         // Aufgabe 2.b
-        Wuerfel w6 = new Wuerfel(6);// ----> Hier rufen wir den Konstruktor der Klasse Wuerfel auf. (6 Seitige Würfel)
+        Wuerfel w6 = new Wuerfel(6);// ----> Hier rufen wir den Konstruktor der Klasse Wuerfel auf. (6 Seitige
+                                    // Würfel)
 
         _name = name;
         _at = 5 + w6.wuerfle();
@@ -25,7 +26,8 @@ public class Gladiator {
     // Aufgabe 2.c
 
     public boolean attacke() {
-        Wuerfel w20 = new Wuerfel(20);// ----> Hier rufen wir den Konstruktor der Klasse Wuerfel auf.
+        Wuerfel w20 = new Wuerfel(20);// ----> Hier rufen wir den Konstruktor der Klasse Wuerfel auf. (20 Seitige
+                                      // Würfel)
 
         int w20Ergebnis = w20.wuerfle(); // ----> Hier speichern wir das Ergebnis des Würfels in der Variable
                                          // w20Ergebnis.
@@ -60,12 +62,17 @@ public class Gladiator {
 
     public boolean nehmeScahden(int tp) { // ----> Hier wird die Methode nehmeSchaden mit dem Parameter int tp
                                           // definiert.
+        int schadenPunkte = tp - _rs; // ----> Hier wird die Variable schadenPunkte definiert. Sie ist gleich dem
+                                      // Parameter tp - dem Attribut _rs.
 
-        if (_le < 5) { // ----> Wenn _le(Lebensenergie) kleiner als 5 ist, kann Gladiator nicht
-                       // kamfähig sein.
+        _le = _le - schadenPunkte; // ----> Hier wird das Attribut _le um die Variable schadenPunkte reduziert.
+
+        if (_le < 5 && _le > 0) { // ----> Wenn _le(Lebensenergie) kleiner als 5 ist, kann Gladiator nicht
+            // kamfähig sein. Ich habe hier && _le > 0 geschrieben, weil wenn _le(Lebensenergie) kleiner als 0 ist, ist Gladiator tot.
 
             System.out.println(this._name + " ist kampfunfähig.");
             return false;
+
         } else if (_le <= 0) {// ----> Wenn _le(Lebensenergie) kleiner oder gleich 0
                               // ist, ist Gladiator tot.
 
@@ -77,14 +84,13 @@ public class Gladiator {
     }
 
     // Aufgabe 2.f
-
     public static void main(String[] args) {
 
         // Erzeuge einen Gladiator mit dem Namen Maximus
-        Gladiator gladiator = new Gladiator("Maximus", 5, 5, 0, 30);
+        Gladiator gladiator = new Gladiator("Maximus", 5, 5, 0, 12);
 
         System.out.println(gladiator.attacke());
         System.out.println(gladiator.parade());
-        System.out.println(gladiator.nehmeScahden(0));
+        System.out.println(gladiator.nehmeScahden(100)); //---> Maximus ist tot. :(
     }
 }
